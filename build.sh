311cf851b9e7731786b388c56928ed1f46365c57
entity <krupicka.adam@gmail.com>
2014-07-01 11:46:02 +0200

    Improved the build.sh script to check if the compilation process has failed.

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 ~/dev/android-ndk-r9d/ndk-build
+if [ $? -ne 0 ]; then
+	echo "Build failed, aborting..."
+	exit -1;
+fi
 ant debug
 adb -d uninstall com.entity.polygame
 adb -d install bin/Polygame-debug.apk
